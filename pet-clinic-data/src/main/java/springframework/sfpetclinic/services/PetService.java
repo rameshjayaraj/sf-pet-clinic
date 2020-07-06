3e37b595085759e42b71673d7090479cbabb4447
Ramesh Jayaraj <rameshjayaraj@users.noreply.github.com>
2020-07-06 18:00:14 +0530

    Implement Interfaces for Services. Closes #5

---
+++ pet-clinic-data/src/main/java/springframework/sfpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package springframework.sfpetclinic.services;
+
+import springframework.sfpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/springframework/sfpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package springframework.sfpetclinic.services;
+
+import springframework.sfpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
