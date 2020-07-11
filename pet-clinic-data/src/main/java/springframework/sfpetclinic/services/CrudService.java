cdb05c095e819afa12d57ed4b41e6842b961adc2
Ramesh Jayaraj <rameshjayaraj@users.noreply.github.com>
2020-07-11 15:42:13 +0530

    refactor common base interface. Closes #11

---
+++ pet-clinic-data/src/main/java/springframework/sfpetclinic/services/CrudService.java
@@ -0,0 +1,15 @@
+package springframework.sfpetclinic.services;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Set;
+
+public interface CrudService<T, ID>  {
+
+    Set<T> findAll();
+    T findById(ID id);
+    T save(T object);
+
+    void delete(T Object);
+    void deleteById(ID id);
+}
